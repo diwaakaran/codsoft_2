83376a7c5937ce42c3a01cb64014d5d9b6d220f9
diwaakaran <155351472+diwaakaran@users.noreply.github.com>
2024-01-16 16:51:56 +0530

    Add files via upload

---
+++ calculator..py
@@ -0,0 +1,57 @@
+a=int(input("enter a number"))
+b=int(input("enter a number"))
+print("\n The Calculator Program \n")
+
+
+add=a+b
+
+if (add):{
+   
+    print(add)
+    
+    }
+
+
+sub=a-b
+
+
+if(sub):{
+   
+    print(sub)
+    }
+
+
+mul=a*b
+
+if(mul):{
+
+    print(mul)
+    }
+
+
+div=a/b
+
+if(div):{
+    
+    print(div)
+
+    }
+
+mod=a%b
+
+if(mod):{
+    print(mod)
+    }
+
+exp=a**b
+
+if(exp):{
+    print(exp)
+    }
+
+fd=a//b
+
+if(fd):{
+    
+    print(fd)
+    }
